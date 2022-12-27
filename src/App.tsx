26b93e173fef2b1c7e9e04bea2fbab5024882c6e
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2022-12-27 20:13:44 +0100

    feat: BMD-4 Add custom typography style

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import "./styling.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>H1 text</h1>
+      <h2>h2 text</h2>
+      <p>Paragraph text</p>
+      <p className="smallerParagraph">Smaller paragraph text</p>
+    </div>
+  );
 }
 
 export default App;
