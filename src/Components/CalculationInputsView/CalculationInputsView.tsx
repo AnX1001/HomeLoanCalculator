e23a5143a9e5c801d062a5d3b44ca09e4cb35123
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2022-12-28 16:47:44 +0100

    BMD-15 Add styled calculationInputsView

---
+++ src/Components/CalculationInputsView/CalculationInputsView.tsx
@@ -0,0 +1,13 @@
+import "./StylingCalculationInputsView.scss";
+
+/* the wrapper grows in height along with content. The width is always as large as window width. */
+
+function CalculationInputsView() {
+  return (
+    <div className="wrapper">
+      <p>Innside wrapper</p>
+    </div>
+  );
+}
+
+export default CalculationInputsView;
