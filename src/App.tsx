14b3e3fbad44adfd6cf196861a11daa6e6a88abe
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2022-12-28 20:13:08 +0100

    feat: BMD-16 Add styled Resultsview

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CalculationInputsView from "./Components/CalculationInputsView/CalculationInputsView";
 import "./Components/MainStyling/MainStyling.scss";
+import ResultsView from "./Components/ResultsView/ResultsView";
 
 function App() {
   return (
     <div className="App">
       <CalculationInputsView />
+      <ResultsView />
     </div>
   );
 }
