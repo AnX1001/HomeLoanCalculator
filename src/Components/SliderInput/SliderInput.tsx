ddc12527f2a37b3f16f6543be5bf126dd9cf8267
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2022-12-29 20:32:10 +0100

    feat: BMD-18 Add styled input & inputslider

--- src/Components/SliderInput/SliderInput.tsx
+++ src/Components/SliderInput/SliderInput.tsx
@@ -1,5 +1,45 @@
-function SliderInput() {
-  return <p>SliderInput</p>;
+import { useState } from "react";
+import styles from "./stylingSliderInput.module.scss";
+interface Props {
+  title: string;
+  inputfieldPlaceholder: string;
 }
 
+function SliderInput({ title, inputfieldPlaceholder }: Props) {
+  const [value, setValue] = useState<number | string>("");
+
+  const handleOnChange = (event: any) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <>
+      <div className={styles.inputGroup}>
+        <label>
+          <h3 className={styles.h3}>{title}</h3>
+          <input
+            name="boligpris"
+            className={styles.inputNumber}
+            placeholder={inputfieldPlaceholder}
+            type="number"
+            min="0"
+            id="boligpris"
+            value={parseInt(value.toString()) ? value : ""}
+            onChange={handleOnChange}
+          />
+        </label>
+
+        <input
+          aria-label="velg sum"
+          className={styles.inputSlider}
+          type="range"
+          min="0"
+          max="100000000"
+          step="1000"
+          onChange={handleOnChange}
+        />
+      </div>
+    </>
+  );
+}
 export default SliderInput;
