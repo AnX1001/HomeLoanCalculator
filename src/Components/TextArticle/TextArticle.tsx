1007dd40e08967fe2914c0649700c189770e725b
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2022-12-30 22:55:01 +0100

    BMD-17 feat: Add TextArticle component with props

--- src/Components/TextArticle/TextArticle.tsx
+++ src/Components/TextArticle/TextArticle.tsx
@@ -1,5 +1,18 @@
-function TextArticle() {
-  return <p>TextArticle</p>;
+interface Props {
+  h1Heading: string;
+  h2Heading: string;
+  paragraph: string;
+}
+
+function TextArticle({ h1Heading, h2Heading, paragraph }: Props) {
+  return (
+    <>
+      <h1>{h1Heading}</h1>
+      <slot></slot>
+      <h2>{h2Heading}</h2>
+      <p>{paragraph}</p>
+    </>
+  );
 }
 
 export default TextArticle;
