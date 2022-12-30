f13f4c8ba91719401d681315b05e05c550252581
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2022-12-30 22:56:08 +0100

    BMD-17 Render TextArticle from CalculationInputsView

--- src/Components/CalculationInputsView/CalculationInputsView.tsx
+++ src/Components/CalculationInputsView/CalculationInputsView.tsx
@@ -1,4 +1,5 @@
 import SliderInput from "../SliderInput/SliderInput";
+import TextArticle from "../TextArticle/TextArticle";
 import styles from "./StylingCalculationInputsView.module.scss";
 
 /* the wrapper grows in height along with content. The width is always as large as window width. */
@@ -6,6 +7,7 @@ import styles from "./StylingCalculationInputsView.module.scss";
 function CalculationInputsView() {
   return (
     <div className={styles.wrapper}>
+      <TextArticle h1Heading="Boliglånskalkulatoren" h2Heading="Hvor mye kan du få i boliglån" paragraph="Dette er avhengig av mange variabler. Men en vanlig kalkulasjon vil ta i betraktning din årsinntekt, gjeld, egenkapital og boligens totalpris."/>
       <SliderInput placeholder="Boligpris" title={"Boligens pris"} />
       <SliderInput placeholder="Egenkapital" title={"Egenkapital"} />
       <SliderInput placeholder="Gjeld" title={"Gjeld"} />
