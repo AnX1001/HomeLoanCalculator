09c18e054bea172e9ce0c6d6d69737d2b875540a
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2022-12-31 13:44:47 +0100

    BMD-19 Add props and image element

--- src/Components/ProportionalImage/ProportionalImage.tsx
+++ src/Components/ProportionalImage/ProportionalImage.tsx
@@ -1,6 +1,14 @@
-function ProportionalImage() {
+interface Props {
+  src: string;
+  alt: string;
+}
+
+function ProportionalImage({ alt, src }: Props) {
   return (
-    
-  )
+    <>
+      <img alt={alt} src={src} />
+    </>
+  );
+}
 
 export default ProportionalImage;
