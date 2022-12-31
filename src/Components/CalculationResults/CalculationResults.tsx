d2ecbecc9619937f799a89db565c82d7181e1604
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2022-12-31 23:17:46 +0100

    BMD-23 feat: add styled calculationResults comp

---
+++ src/Components/CalculationResults/CalculationResults.tsx
@@ -0,0 +1,21 @@
+import styles from './StylingCalculationResults.module.scss'
+
+interface Props {
+    smallHeading: string;
+    largeHeading: string;
+    smallerHeading: string;
+    SimpleParagraph?: React.ReactNode;
+}
+
+function CalculationResults({ smallHeading, largeHeading, SimpleParagraph }: Props) {
+    return (
+        <div className={styles.wrapper}>
+            <p className={styles.smallHeading }>{smallHeading} </p>
+            <p className={styles.largeHeading}>{largeHeading} kr</p>
+        {SimpleParagraph}
+        </div>
+
+    )
+}
+
+export default CalculationResults
--- src/Components/ResultsView/ResultsView.tsx
+++ src/Components/ResultsView/ResultsView.tsx
@@ -1,9 +1,13 @@
+import CalculationResults from "../CalculationResults/CalculationResults";
 import styles from "./StylingResultsView.module.scss";
+import SimpleParagraph from ".././SimpleParagraph/SimpleParagraph"
 
 function ResultsView() {
   return (
     <div className={styles.wrapper}>
-      <p>Resultsview</p>
+      <CalculationResults smallHeading="Innvilget lån" largeHeading="99.0000" smallerHeading={"Husk at:"} />
+      <CalculationResults smallHeading="Innvilget lån" largeHeading="99.0000" smallerHeading={"Husk at:"} />
+      <SimpleParagraph smallerHeading="Husk at:" />
     </div>
   );
 }
