5493518c0ff463902e717fa965358c5d7a970bc0
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-03-12 22:03:05 +0100

    chore: add updated package-lock.json

--- src/Components/CalculationResults/CalculationResults.tsx
+++ src/Components/CalculationResults/CalculationResults.tsx
@@ -1,17 +1,17 @@
 import styles from './StylingCalculationResults.module.scss'
 
-interface CalculationResultsProps  {
-  smallHeading: string;
-  totalAmount: number;
-  content?: React.ReactNode;
+interface CalculationResultsProps {
+    smallHeading: string;
+    totalAmount: number;
+    content?: React.ReactNode;
 }
 
-function CalculationResults({ smallHeading, totalAmount, content }: CalculationResultsProps ) {
+function CalculationResults({ smallHeading, totalAmount, content }: CalculationResultsProps) {
     return (
         <div className={styles.wrapper}>
-            <p className={styles.title }>{smallHeading} </p>
+            <p className={styles.title}>{smallHeading} </p>
             <p className={styles.amount}>{totalAmount} kr</p>
-        {content}
+            {content}
         </div>
 
     )
