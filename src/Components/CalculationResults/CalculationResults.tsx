0c5d681dfceb062dd3dbcee0dbaa4898e28da671
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-03-16 15:38:37 +0100

    fix: use babel plugin proposal

--- src/Components/CalculationResults/CalculationResults.tsx
+++ src/Components/CalculationResults/CalculationResults.tsx
@@ -5,11 +5,15 @@ interface Props {
   amount: number;
 }
 
+
+
 function CalculationResults({ heading, amount }: Props) {
+  const formattedAmount = new Intl.NumberFormat('no-NO').format(amount);
+
   return (
     <section className={styles.wrapper}>
       <h1 className={styles.heading}>{heading} </h1>
-      <p className={styles.amount}>{amount} kr</p>
+      <p className={styles.amount}>{formattedAmount} kr</p>
     </section>
   );
 }
