34d2369d520c46df4882324a8937c0a7754be717
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-03-16 14:00:56 +0100

    chore: fix merge conflicts

--- src/Components/ResultsView/ResultsView.tsx
+++ src/Components/ResultsView/ResultsView.tsx
@@ -14,21 +14,12 @@ function ResultsView({ loanNeed, eligibleLoan }: ResultsViewProps) {
   return (
     <div className={styles.wrapper}>
       <CalculationResults
-<<<<<<< HEAD
         heading="Innvilget lån"
         amount={eligibleLoan}
       />
       <CalculationResults
         heading="Ditt lånebehov"
         amount={loanNeed}
-=======
-        smallHeading="Innvilget lån"
-        totalAmount={eligibleLoan < 0 ? 0 : eligibleLoan}
-      />
-      <CalculationResults
-        smallHeading="Ditt lånebehov"
-        totalAmount={loanNeed < 0 ? 0 : loanNeed}
->>>>>>> ec6acddad70ff26d812956dc6d9e880a26c266b9
       />
       <SimpleParagraph smallerHeading="Husk at:" />
     </div>
