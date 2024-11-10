e428b706a024c88e75432389f789323515aff8a1
An <50742694+AnX1001@users.noreply.github.com>
2024-11-10 14:03:13 +0100

    feat: add info text

--- src/features/summary/Result.tsx
+++ src/features/summary/Result.tsx
@@ -8,9 +8,9 @@ interface ResultProps {
 
 function Result({ heading, amount, approvedLoan }: ResultProps) {
   
-  const approvedLoanLabelText = amount > 1 ? new Intl.NumberFormat("no-NO").format(amount) + ` kr` : 0;
+  const approvedLoanLabelText = amount > 1 ? new Intl.NumberFormat("no-NO").format(amount) + ` kr` : 'Lån ikke godkjent';
 
-  const loanRequirementsText = amount > 1 ? new Intl.NumberFormat("no-NO").format(amount) + ` kr` : 0;
+  const loanRequirementsText = amount > 1 ? new Intl.NumberFormat("no-NO").format(amount) + ` kr` : 'Ingen lån nødvendig';
   return (
     <section className={styles.wrapper}> 
       <h2 className={styles.heading}>{heading} </h2>
