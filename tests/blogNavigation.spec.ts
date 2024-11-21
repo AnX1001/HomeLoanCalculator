16ebabc746965c621344bca432e4412914aae74e
An <50742694+AnX1001@users.noreply.github.com>
2024-11-21 22:29:22 +0100

    test: split navigation tests for blog and home page

--- tests/blogNavigation.spec.ts
+++ tests/blogNavigation.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
+
 test.use({ launchOptions: { slowMo: 5000 } });
+
 test("should navigate to the blog page when the blog link is clicked", async ({
   page,
 }) => {
@@ -7,5 +9,16 @@ test("should navigate to the blog page when the blog link is clicked", async ({
   await page.getByRole("link", { name: "Blogg" }).click();
   await expect(
     page.getByRole("heading", { name: "Blog tittel ipsum, dolor sit amet" }),
+  ).toBeVisible();
+});
+
+test("should navigate to the home page when the home link is clicked", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3000/");
+  await page.getByRole("link", { name: "Blogg" }).click();
+  await page.getByRole("link", { name: "Hjem" }).click();
+  await expect(
+    page.getByRole("heading", { name: "Lån ikke godkjent" }),
   ).toBeVisible();
 });
