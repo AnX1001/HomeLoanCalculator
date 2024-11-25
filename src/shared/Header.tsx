ceac3927e8956fb7c8c7de409ec12bfd78258dac
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2024-11-25 23:19:36 +0100

    feat: add protected route

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -2,12 +2,31 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import LoginFormModal from "./LoginModalForm";
 import style from "./header.module.scss";
+import { useAuth } from "../firebase/AuthProvider";
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 
 
 function Header() {
   const [open, setOpen] = useState(false);
+  const { user } = useAuth();
 
-  
+
+
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("Sign-out error:", error.message);
+      });
+  };
+
 
   return (
     <header className={style.header}>
@@ -23,8 +42,8 @@ function Header() {
             <Link to={"/minside"}>Min side</Link>
           </li>
           <li>
-            <button className={style.loginButton} onClick={() => setOpen((prevState) => !prevState)}>
-              🔒 Logg på
+            <button className={style.loginButton} onClick={!user ? () => setOpen((prevState) => !prevState) : handleSignOut}>
+              {user ? "🔓 Logg ut" : "🔒 Logg på"}
             </button>
             <LoginFormModal setOpen={setOpen} open={open} />
           </li>
