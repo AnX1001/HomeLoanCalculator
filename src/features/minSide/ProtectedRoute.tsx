3a35db950b4092fdb88bf57c149112c0f6c6b427
An <50742694+AnX1001@users.noreply.github.com>
2024-11-26 23:01:14 +0100

    chore: outcomment echo

--- src/features/minSide/ProtectedRoute.tsx
+++ src/features/minSide/ProtectedRoute.tsx
@@ -5,18 +5,19 @@ import MinSide from "./MinSide";
 function ProtectedRoute() {
   const { user, loading } = useAuth();
 
-  if(!firebaseInitialized) {
-    return <p style={{padding: "20px"}}>Ingen autentisering tilgjengelig.</p>;
+  if (!firebaseInitialized) {
+    return <p style={{ padding: "20px" }}>Ingen autentisering tilgjengelig.</p>;
   }
   if (loading) {
-    return <p style={{margin: "20px"}}>Laster siden...</p>;
+    return <p style={{ margin: "20px" }}>Laster siden...</p>;
   }
 
-
-
-
   if (!user) {
-    return <p style={{padding: "20px"}}>Du må være innlogget for å se denne siden.</p>;
+    return (
+      <p style={{ padding: "20px" }}>
+        Du må være innlogget for å se denne siden.
+      </p>
+    );
   }
 
   return <MinSide />;
