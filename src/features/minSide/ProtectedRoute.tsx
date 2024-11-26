4ff807a6f4a36de727e6968eeba131c53528a5d6
An <50742694+AnX1001@users.noreply.github.com>
2024-11-26 23:02:21 +0100

    An/ci playwright wo auth (#53)
    * test: run playW wo auth
    
    * chore: outcomment echo

--- src/features/minSide/ProtectedRoute.tsx
+++ src/features/minSide/ProtectedRoute.tsx
@@ -1,17 +1,23 @@
 import { useAuth } from "../../firebase/AuthProvider";
+import { firebaseInitialized } from "../../firebase/firebase";
 import MinSide from "./MinSide";
 
 function ProtectedRoute() {
   const { user, loading } = useAuth();
 
-
+  if (!firebaseInitialized) {
+    return <p style={{ padding: "20px" }}>Ingen autentisering tilgjengelig.</p>;
+  }
   if (loading) {
-    return <p>Loading...</p>;
+    return <p style={{ margin: "20px" }}>Laster siden...</p>;
   }
 
-
   if (!user) {
-    return <p style={{padding: "20px"}}>Du må være innlogget for å se denne siden.</p>;
+    return (
+      <p style={{ padding: "20px" }}>
+        Du må være innlogget for å se denne siden.
+      </p>
+    );
   }
 
   return <MinSide />;
--- src/shared/LoginModalForm.tsx
+++ src/shared/LoginModalForm.tsx
@@ -1,7 +1,8 @@
-import {useState} from "react";
+import { useState } from "react";
 import useEscapeKey from "./useScapeKey";
 import style from "./LoginModalForm.module.scss";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseInitialized } from "../firebase/firebase";
 
 interface LoginModalFormProps {
   open: boolean;
@@ -10,34 +11,34 @@ interface LoginModalFormProps {
 
 function LoginModalForm({ open, setOpen }: LoginModalFormProps) {
   const [emailError, setEmailError] = useState(false);
+  useEscapeKey(() => setOpen(false), open);
 
-
+  if (!firebaseInitialized) {
+    {
+      console.warn(
+        "Firebase environment variables are not set. Skipping Firebase initialization.",
+      );
+      return;
+    }
+  }
   const auth = getAuth();
-
-
- 
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const form = event.currentTarget; 
+    const form = event.currentTarget;
     const formData = new FormData(form);
 
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
     if (!email.includes("@")) {
-     
       setEmailError(true);
       return;
     }
 
-    
-    
     signInWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        
-      })
+      .then(() => {})
       .catch((error) => {
         console.error("Login error:", error.code, error.message);
       });
@@ -45,24 +46,23 @@ function LoginModalForm({ open, setOpen }: LoginModalFormProps) {
     setOpen(false);
   };
 
-
-  useEscapeKey(() => setOpen(false), open);
-
-
-
-
-
-
   return (
     <dialog className={style.dialog} open={open}>
       <button onClick={() => setOpen(false)} className={style.closeIconButton}>
         &#x2716;
       </button>
-      <form
-        onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email">E-post</label>
-        <input type="text" id="email" name="email" required onChange={()=> setEmailError(false)} />
-        <p style={{color: "red", fontSize: "small", marginTop: "-15px"}}>{emailError && "Vennligst oppgi gyldig email"}</p>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          required
+          onChange={() => setEmailError(false)}
+        />
+        <p style={{ color: "red", fontSize: "small", marginTop: "-15px" }}>
+          {emailError && "Vennligst oppgi gyldig email"}
+        </p>
         <label htmlFor="password">Passord</label>
         <input
           type="password"
