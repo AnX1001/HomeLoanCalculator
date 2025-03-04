c6dd60ddc69dc57ad9150d74ab95a09bfec9f99c
AnX1001 <50742694+AnX1001@users.noreply.github.com>
2025-03-04 21:52:45 +0100

    feat: show translation on language

--- src/features/home/summary/Result.tsx
+++ src/features/home/summary/Result.tsx
@@ -1,21 +1,25 @@
 import styles from "./result.module.scss";
-
+import textContent from "../../../mortageContent/mortageContent.json";
+import { userLocale } from "../../../utils/languageUtil";
 interface ResultProps {
   heading: string;
   amount: number;
   approvedLoan?: boolean;
 }
 
+
+const { result: { subheading: { loanNotApproved, loanNotNecessary } } } = textContent;
+
 function Result({ heading, amount, approvedLoan }: ResultProps) {
   const approvedLoanLabelText =
     amount > 1
       ? new Intl.NumberFormat("no-NO").format(amount) + ` kr`
-      : "Lån ikke godkjent";
+      : loanNotApproved[userLocale];
 
   const loanRequirementsText =
     amount > 1
       ? new Intl.NumberFormat("no-NO").format(amount) + ` kr`
-      : "Ingen lån nødvendig";
+      : loanNotNecessary[userLocale];
   return (
     <section className={styles.wrapper}>
       <h2 className={styles.heading}>{heading} </h2>
